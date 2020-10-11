a276ce0cf8979ab5eb04a476485b6cd8b45823b7
Damian Nguyen <damian.nguyen93@gmail.com>
2020-10-11 12:06:07 +0200

    v2 of using model and dto - lists

--- src/main/java/com/damiannguyen/modules/home/HomeController.java
+++ src/main/java/com/damiannguyen/modules/home/HomeController.java
@@ -5,6 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class HomeController {
 //    @RequestMapping("/")
@@ -12,11 +15,21 @@ public class HomeController {
     @RequestMapping( method = RequestMethod.GET, path = {"/", "index.html"})
     public String getHome(Model model) {
         PersonDto person = new PersonDto("Damian", "Nguyen", 27);
+        PersonDto person2 = new PersonDto("Paulina", "Buczkowska", 26);
+        PersonDto person3 = new PersonDto("Iliana", "Parasiak", 8);
 
+        List<PersonDto> personDtoList = List.of(person, person2, person3);
+
+        //Polami
 //        model.addAttribute("name", person.getName());
 //        model.addAttribute("surname", person.getSurname());
 //        model.addAttribute("age", person.getAge());
-        model.addAttribute("person", person);
+
+        //Obiektami
+//        model.addAttribute("person", person);
+
+        //Listami
+        model.addAttribute("personList", personDtoList);
 
         return "home";
     }
