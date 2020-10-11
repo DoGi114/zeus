35d3f3329cbf5b04f14efb2f711ffb3a4f8c7b3a
Damian Nguyen <damian.nguyen93@gmail.com>
2020-10-11 11:23:38 +0200

    Add hello world controller and view v2

--- src/main/java/com/damiannguyen/modules/home/HomeController.java
+++ src/main/java/com/damiannguyen/modules/home/HomeController.java
@@ -1,12 +1,17 @@
 package com.damiannguyen.modules.home;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/")
-    public String getHome() {
+//    @RequestMapping("/")
+//    @RequestMapping( method = RequestMethod.GET, path = "/")
+    @RequestMapping( method = RequestMethod.GET, path = {"/", "index.html"})
+    public String getHome(Model model) {
+        model.addAttribute("name", "Damian");
         return "home";
     }
 }
